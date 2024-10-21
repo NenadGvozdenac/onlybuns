ec3b13067dd45c6822adc841f6fde4a7398af794
NenadGvozdenac <nenadgvozdenacsrb@gmail.com>
2024-10-21 16:13:50 +0200

    added email verification via email controller.

---
+++ onlybuns-be/src/main/java/com/onlybuns/onlybuns/presentation/dtos/requests/EmailDto.java
@@ -0,0 +1,12 @@
+package com.onlybuns.onlybuns.presentation.dtos.requests;
+
+import lombok.Data;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class EmailDto {
+    private String email;
+}
