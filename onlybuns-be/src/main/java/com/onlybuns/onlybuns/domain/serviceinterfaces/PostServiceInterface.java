c02998fff4ed4b6f34a157b3a90ae2d0ce5c9c36
Mihajlo Sremac <92401223+MihajloSremac@users.noreply.github.com>
2024-10-22 15:30:24 +0200

    Added Images to Posts

--- onlybuns-be/src/main/java/com/onlybuns/onlybuns/domain/serviceinterfaces/PostServiceInterface.java
+++ onlybuns-be/src/main/java/com/onlybuns/onlybuns/domain/serviceinterfaces/PostServiceInterface.java
@@ -3,11 +3,12 @@ package com.onlybuns.onlybuns.domain.serviceinterfaces;
 import java.util.List;
 
 import com.onlybuns.onlybuns.core.misc.Result;
+import com.onlybuns.onlybuns.presentation.dtos.requests.UpdatePostDto;
 import com.onlybuns.onlybuns.presentation.dtos.responses.PostDto;
 
 public interface PostServiceInterface {
     public Result<PostDto> likePost(Long postId, String userUsername);
     public Result<List<PostDto>> getAllPosts();
     public Result<String> deletePost(Long postId, String userUsername);
-    public Result<PostDto> updatePost(PostDto postDto, String userUsername);
+    public Result<PostDto> updatePost(UpdatePostDto updatePostDto, String userUsername);
 }
