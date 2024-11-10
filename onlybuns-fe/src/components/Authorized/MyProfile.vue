4c1265be3548898d16b6604a85408b7b8339f9c4
Nenad Gvozdenac <nenadgvozdenacsrb@gmail.com>
2024-11-10 10:57:44 +0100

    fixed profile redirection. added map to update profile.

--- onlybuns-fe/src/components/Authorized/MyProfile.vue
+++ onlybuns-fe/src/components/Authorized/MyProfile.vue
@@ -2,105 +2,112 @@
     <Navbar />
 
     <div class="container mt-4">
-        <div class="row">
-            <!-- Left Column: User Details -->
-            <div class="col-12">
-                <div class="user-card shadow-lg mt-3">
-                    <div class="d-flex justify-content-end">
-                        <button class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#editProfileModal">
-                            Update Profile
-                        </button>
-                    </div>
-                    <div class="user-card-logo">
-                        <img src="https://flowbite.com/docs/images/logo.svg" alt="Twitter Logo" class="h-8 me-2">
-                    </div>
-                    <div class="text-center mt-3 user-name">
-                        Onlybuns User
-                    </div>
-                    <h4 class="user-card-title">{{ user.name }} {{ user.surname }}</h4>
-                    <div class="user-info-table">
-                        <div class="user-info-row">
-                            <span class="user-info-label">Username:</span>
-                            <span class="user-info-value">{{ user.username }}</span>
+        <div class="profile-container">
+            <div class="profile-card">
+                <div class="shadow-lg mt-3 border-0 rounded-4">
+                    <!-- Background Banner -->
+                    <div class="banner bg-primary bg-gradient text-white rounded-top-4"></div>
+
+                    <div class="card-body p-4 position-relative">
+                        <!-- Profile Header -->
+                        <div class="text-center">
+                            <!-- Profile Image -->
+                            <img src="https://flowbite.com/docs/images/logo.svg" alt="Profile Logo"
+                                class="profile-image">
+
+                            <h3 class="fw-bold mb-1">{{ user.name }} {{ user.surname }}</h3>
+                            <p class="username-text">@{{ user.username }}</p>
+
+                            <button class="edit-button" data-bs-toggle="modal" data-bs-target="#editProfileModal">
+                                <i class="bi bi-pencil-fill me-2"></i>Edit Profile
+                            </button>
                         </div>
-                        <div class="user-info-row">
-                            <span class="user-info-label">Email:</span>
-                            <span class="user-info-value">{{ user.email }}</span>
+
+                        <!-- Stats Row -->
+                        <div class="stats-container">
+                            <div class="stat-item">
+                                <div class="stat-content border-end">
+                                    <h4 class="fw-bold mb-1">{{ user.followers.length }}</h4>
+                                    <p class="stat-label">Followers</p>
+                                </div>
+                            </div>
+                            <div class="stat-item">
+                                <div class="stat-content">
+                                    <h4 class="fw-bold mb-1">{{ user.following.length }}</h4>
+                                    <p class="stat-label">Following</p>
+                                </div>
+                            </div>
                         </div>
-                        <div class="user-info-row">
-                            <span class="user-info-label">Address:</span>
-                            <span class="user-info-value">
-                                {{ user.address.street }} {{ user.address.number }}, {{ user.address.city }}, {{
-                                    user.address.country }}
-                            </span>
-                        </div>
-                        <div class="user-info-row">
-                            <span class="user-info-label">Following:</span>
-                            <span class="user-info-value">{{ user.following.length }}</span>
-                        </div>
-                        <div class="user-info-row">
-                            <span class="user-info-label">Followers:</span>
-                            <span class="user-info-value">{{ user.followers.length }}</span>
+
+                        <!-- User Info -->
+                        <div class="user-info">
+                            <div class="info-row">
+                                <div class="icon-container">
+                                    <i class="bi bi-envelope text-primary fs-5"></i>
+                                </div>
+                                <div>
+                                    <div class="info-label">Email</div>
+                                    <div class="info-value">{{ user.email }}</div>
+                                </div>
+                            </div>
+
+                            <div class="info-row">
+                                <div class="icon-container">
+                                    <i class="bi bi-geo-alt text-primary fs-5"></i>
+                                </div>
+                                <div>
+                                    <div class="info-label">Address</div>
+                                    <div class="info-value">
+                                        {{ user.address.street }} {{ user.address.number }}, {{ user.address.city }}, 
+                                        {{ user.address.country }}
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
+        </div>
 
-
-            <!-- Right Column: User Posts -->
-            <div>
-                <div v-if="user.activePosts.length" class="d-flex col-12 gap-3">
-                    <div class="row">
-                        <div class="col-md-4">
-                            <div v-for="(card, index) in user.activePosts.filter((_, i) => i % 3 === 0)" :key="index">
-                                <ProfileCardsComponent :key="card.id" :id="card.id" :image="card.image"
-                                    :likesCount="card.numberOfLikes" :description="card.description"
-                                    :commentsCount="card.comments.length" :username="user.username"
-                                    :dateOfCreation="card.dateOfCreation" :usersThatLike="card.users"
-                                    :comments="card.comments" />
-                            </div>
-                        </div>
-
-                        <div class="col-md-4">
-                            <div v-for="(card, index) in user.activePosts.filter((_, i) => i % 3 === 1)" :key="index">
-                                <ProfileCardsComponent :key="card.id" :id="card.id" :image="card.image"
-                                    :likesCount="card.numberOfLikes" :description="card.description"
-                                    :commentsCount="card.comments.length" :username="user.username"
-                                    :dateOfCreation="card.dateOfCreation" :usersThatLike="card.users"
-                                    :comments="card.comments" />
-                            </div>
-                        </div>
-                        <div class="col-md-4">
-                            <div v-for="(card, index) in user.activePosts.filter((_, i) => i % 3 === 2)" :key="index">
-                                <ProfileCardsComponent :key="card.id" :id="card.id" :image="card.image"
-                                    :likesCount="card.numberOfLikes" :description="card.description"
-                                    :commentsCount="card.comments.length" :username="user.username"
-                                    :dateOfCreation="card.dateOfCreation" :usersThatLike="card.users"
-                                    :comments="card.comments" />
-                            </div>
+        <!-- Posts Grid -->
+        <div class="posts-container">
+            <div v-if="user.activePosts.length" class="posts-grid">
+                <div class="row">
+                    <div class="col-md-4" v-for="colIndex in 3" :key="colIndex">
+                        <div v-for="(card, index) in user.activePosts.filter((_, i) => i % 3 === colIndex - 1)" :key="index">
+                            <ProfileCardsComponent 
+                                :key="card.id" 
+                                :id="card.id" 
+                                :image="card.image"
+                                :likesCount="card.numberOfLikes" 
+                                :description="card.description"
+                                :commentsCount="card.comments.length" 
+                                :username="user.username"
+                                :dateOfCreation="card.dateOfCreation" 
+                                :usersThatLike="card.users"
+                                :comments="card.comments" 
+                            />
                         </div>
                     </div>
                 </div>
-                <div v-else>
-                    <p>No posts available</p>
-                </div>
+            </div>
+            <div v-else class="no-posts">
+                <p>No posts available</p>
             </div>
         </div>
     </div>
 
-    <!-- Add the Edit Profile Modal -->
+    <!-- Edit Profile Modal -->
     <EditProfileModal :userData="user" @update-profile="updateProfile" ref="editProfileModal" />
 
     <Footer />
 </template>
 
 <script>
-
 import Navbar from './Navbar.vue';
 import Footer from '../Unauthorized/Footer.vue';
 import ProfileCardsComponent from '../Cards/ProfileCardsComponent.vue';
 import EditProfileModal from '../Layout/EditProfileModal.vue';
-
 import ProfileService from '@/services/ProfileService';
 
 export default {
@@ -124,86 +131,9 @@ export default {
                     city: "",
                     country: ""
                 },
-                activePosts: [
-                    {
-                        id: 0,
-                        "users": [
-                            {
-                                username: "string",
-                                name: "string",
-                                surname: "string",
-                                email: "string",
-                                address: {
-                                    street: "string",
-                                    number: 0,
-                                    city: "string",
-                                    country: "string"
-                                }
-                            }
-                        ],
-                        image: {
-                            data: "",
-                            mimeType: "",
-                            uploadedAt: [
-                                2024,
-                                10,
-                                21,
-                                15,
-                                30
-                            ]
-                        },
-                        dateOfCreation: [
-                            2024,
-                            10,
-                            19,
-                            13,
-                            32
-                        ],
-                        description: "",
-                        numberOfLikes: 0,
-                        comments: [
-                            {
-                                id: 0,
-                                comment: "string",
-                                commentedAt: [
-                                    2024,
-                                    10,
-                                    19,
-                                    13,
-                                    32
-                                ],
-                            }
-                        ]
-                    }
-                ],
-                followers: [
-                    {
-                        username: "",
-                        name: "",
-                        surname: "",
-                        email: "",
-                        address: {
-                            street: "",
-                            number: 0,
-                            city: "",
-                            country: ""
-                        }
-                    }
-                ],
-                following: [
-                    {
-                        username: "",
-                        name: "",
-                        surname: "",
-                        email: "",
-                        address: {
-                            street: "",
-                            number: 0,
-                            city: "",
-                            country: ""
-                        }
-                    }
-                ]
+                activePosts: [],
+                followers: [],
+                following: []
             }
         }
     },
@@ -220,8 +150,8 @@ export default {
                     console.log(error);
                 });
         },
-
         updateProfile(userData) {
+            console.log(userData)
             ProfileService.updateProfile(userData)
                 .then(response => {
                     this.getUser();
@@ -233,80 +163,130 @@ export default {
         }
     }
 }
-
 </script>
 
-<style>
+<style scoped>
 @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap');
+@import url("https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.0/font/bootstrap-icons.css");
 
-* {
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-    font-family: 'Poppins', sans-serif;
-}
-
-.user-card {
-    max-width: 750px;
-    min-height: 300px;
-    padding: 50px 20px;
-    background-color: #ecf0f3;
-    border-radius: 55px;
-    box-shadow: 10px 10px 20px #cbced1, -10px -10px 20px #fff;
-    text-align: center;
-    margin: auto;
-}
-
-.user-card-logo {
-    width: 60px;
-    margin: 0 auto 10px;
-}
-
-.user-card-logo img {
+.profile-container {
+    display: flex;
+    justify-content: center;
     width: 100%;
-    height: auto;
-    object-fit: cover;
+}
+
+.profile-card {
+    width: 100%;
+    max-width: 750px;
+}
+
+.banner {
+    height: 100px;
+}
+
+.profile-image {
+    width: 120px;
+    height: 120px;
     border-radius: 50%;
-    box-shadow: 0px 0px 3px #5f5f5f, 0px 0px 0px 5px #ecf0f3, 5px 5px 10px #a7aaa7, -5px -5px 10px #fff;
+    border: 4px solid white;
+    box-shadow: 0 .125rem .25rem rgba(0,0,0,.075);
+    position: relative;
+    margin-top: -60px;
+    margin-bottom: 1rem;
 }
 
-.user-name {
-    font-weight: 600;
-    font-size: 1.2rem;
-    color: #555;
-    margin-bottom: 15px;
+.username-text {
+    color: #6c757d;
+    font-size: 0.875rem;
+    margin-bottom: 1rem;
 }
 
-.user-card-title {
-    font-size: 1.3rem;
+.edit-button {
+    background-color: #0d6efd;
+    color: white;
+    border: none;
+    padding: 0.5rem 1.5rem;
+    border-radius: 50rem;
     font-weight: 500;
-    color: #333;
-    margin-bottom: 15px;
+    box-shadow: 0 .125rem .25rem rgba(0,0,0,.075);
+    transition: all 0.3s ease;
 }
 
-.user-info-table {
+.edit-button:hover {
+    background-color: #0b5ed7;
+    transform: translateY(-2px);
+}
+
+.stats-container {
     display: flex;
-    flex-direction: column;
-    gap: 10px;
-    margin-top: 20px;
-    text-align: left;
+    justify-content: center;
+    margin: 2rem 0;
 }
 
-.user-info-row {
+.stat-item {
+    width: 33.333%;
+    text-align: center;
+}
+
+.stat-content {
+    padding: 1rem;
+}
+
+.stat-label {
+    color: #6c757d;
+    font-size: 0.75rem;
+    margin: 0;
+}
+
+.user-info {
+    background-color: #f8f9fa;
+    border-radius: 0.5rem;
+    padding: 1.5rem;
+}
+
+.info-row {
     display: flex;
-    justify-content: space-between;
-    padding: 10px 5px;
-    border-bottom: 1px solid #cbced1;
+    align-items: center;
+    margin-bottom: 1rem;
 }
 
-.user-info-label {
+.info-row:last-child {
+    margin-bottom: 0;
+}
+
+.icon-container {
+    margin-right: 1rem;
+}
+
+.info-label {
+    color: #6c757d;
+    font-size: 0.75rem;
+}
+
+.info-value {
     font-weight: 500;
-    color: #333;
 }
 
-.user-info-value {
-    color: #666;
-    text-align: right;
-    flex: 1;
+.posts-container {
+    margin-top: 2rem;
+}
+
+.posts-grid {
+    width: 100%;
+}
+
+.no-posts {
+    text-align: center;
+    color: #6c757d;
+    margin-top: 2rem;
+}
+
+/* Card hover effect */
+:deep(.card) {
+    transition: all 0.3s ease;
+}
+
+:deep(.card:hover) {
+    transform: translateY(-5px);
 }
 </style>
